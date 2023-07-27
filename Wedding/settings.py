5dfa20badca7edec0cb9ec99026e9045c8cfb2cd
mbtaylor01 <119759844+mbtaylor01@users.noreply.github.com>
2023-07-26 19:09:11 -0500

    Change APP_DIRS in settings.py to True

--- Wedding/settings.py
+++ Wedding/settings.py
@@ -53,7 +53,7 @@ TEMPLATES = [
         'DIRS': [
             BASE_DIR / "WeddingWebsite" / "static" / "templates",
         ],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
