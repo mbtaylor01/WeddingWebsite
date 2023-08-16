89218d0fb98bb6fbb970d1541b4099fd4485502c
mbtaylor01 <119759844+mbtaylor01@users.noreply.github.com>
2023-08-15 23:23:44 -0500

    Change DATABASES in settings.py to read from secrets.json to accommodate sqlite in dev and PostgreSQL in prod

--- Wedding/settings.py
+++ Wedding/settings.py
@@ -72,10 +72,7 @@ WSGI_APPLICATION = 'Wedding.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {key:eval(value) for (key,value) in SECRETS["databases_default"].items()}
 }
 
 
