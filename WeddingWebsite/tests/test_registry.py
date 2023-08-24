3a9bc4b9af185e9af252d6f0bab43161b5592e69
mbtaylor01 <119759844+mbtaylor01@users.noreply.github.com>
2023-08-23 23:39:33 -0500

    Add more tests for the Registry

--- WeddingWebsite/tests/test_registry.py
+++ WeddingWebsite/tests/test_registry.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from WeddingWebsite.models import CustomUser, RSVP, RegistryEntry
+from WeddingWebsite.models import CustomUser, RegistryEntry
 from model_bakery import baker
 
 class RegistryPageTests(TestCase):
@@ -33,12 +33,12 @@ class RegistryPageTests(TestCase):
         self.assertTemplateNotUsed(response, "registry.html")
 
     def test_template_content(self):
-        baker.make(RegistryEntry, title="registry_item_1", _create_files=True)
+        baker.make(RegistryEntry, title="registry_item_1")
         response = self.cli.get(reverse("registry"))
         self.assertContains(response, "registry_item_1")
         self.assertContains(response, "Reserve")
         self.assertNotContains(response, "RESERVED - Click to Unreserve")
-        baker.make(RegistryEntry, title="registry_item_2", reserved_by=self.user, _create_files=True)
+        baker.make(RegistryEntry, title="registry_item_2", reserved_by=self.user)
         response = self.cli.get(reverse("registry"))
         self.assertContains(response, "RESERVED - Click to Unreserve")
 
@@ -50,14 +50,56 @@ class RegistryPostTests(TestCase):
         cls.cli.login(username='testuser', password='12345')
 
     def test_user_reserving_item_when_logged_in(self):
+        baker.make(RegistryEntry, id=1, title="registry_item_1")
         response = self.cli.post(
-            reverse("rsvp"), 
-            data={
-                "additional_people": "5",
-                "allergies": "none",
-                "alcohol": "test",
-                "other": "testtesttest",
-            },
+            reverse("reserve-item"), 
+            data={"item_id": "1",},
+            follow=True
+        )
+        self.assertRedirects(
+            response, 
+            expected_url=reverse("registry"),
+            status_code=302,
+            target_status_code=200,
+        )
+        self.assertNotContains(response, "Reserve")
+        self.assertContains(response, "RESERVED - Click to Unreserve")
+
+    def test_user_unreserving_item_when_logged_in(self):
+        baker.make(RegistryEntry, id=1, title="registry_item_1", reserved_by=self.user)
+        response = self.cli.post(
+            reverse("reserve-item"), 
+            data={"item_id": "1",},
+            follow=True
+        )
+        self.assertRedirects(
+            response, 
+            expected_url=reverse("registry"),
+            status_code=302,
+            target_status_code=200,
+        )
+        self.assertNotContains(response, "RESERVED - Click to Unreserve")
+        self.assertContains(response, "Reserve")
+
+    def test_user_reserving_item_when_not_logged_in(self):
+        self.cli.logout()
+        response = self.cli.post(
+            reverse("reserve-item"), 
+            data={"item_id": "1",},
+        )
+        self.assertRedirects(
+            response, 
+            expected_url="/accounts/login/?next=/reserve-item",
+            status_code=302,
+            target_status_code=200,
+        )
+
+    def test_user_reserving_another_users_item(self):
+        user = CustomUser.objects.create_user(username='testuser2', password='12345')
+        baker.make(RegistryEntry, id=1, title="registry_item_1", reserved_by=user)
+        response = self.cli.post(
+            reverse("reserve-item"), 
+            data={"item_id": "1",},
         )
         self.assertRedirects(
             response, 
@@ -65,4 +107,5 @@ class RegistryPostTests(TestCase):
             status_code=302,
             target_status_code=200,
         )
-        assert len(RSVP.objects.all()) == 1
+
+        
