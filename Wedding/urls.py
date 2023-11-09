21d727867ff9829f263f66994cabe6e20c312220
Mark Taylor <marktaylor1@protonmail.com>
2023-11-08 23:22:28 -0600

    Add logic to urls.py to allow Django to serve media files in development

--- Wedding/urls.py
+++ Wedding/urls.py
@@ -20,9 +20,15 @@ from django.urls import path, include
 from django.conf.urls import handler404
 from WeddingWebsite.views import error_404
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 handler404 = error_404
 
 urlpatterns = [
     path(secret_info.admin_url, admin.site.urls),
     path("", include("WeddingWebsite.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
