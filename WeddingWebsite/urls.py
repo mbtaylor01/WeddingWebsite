a5872e6497654227fc0db37a755b5555e63576b1
Mark Taylor <marktaylor1@protonmail.com>
2023-12-08 16:31:33 -0600

    Prettify urls.py (app version)

--- WeddingWebsite/urls.py
+++ WeddingWebsite/urls.py
@@ -1,23 +1,31 @@
-from .views import HomePageView, InfoPageView, RSVPPageView, LogoutView, AccountInfoView, RegistryListView, RegistryPostView
-from .views import ThreadListView, PostListView, CreateThreadView, ChangeProfilePic, PasswordChangeSuccessView, EditPostView
+from .views import (
+    HomePageView, InfoPageView, RSVPPageView, LogoutView, AccountInfoView, 
+    RegistryListView, RegistryPostView, ThreadListView, PostListView, 
+    CreateThreadView, ChangeProfilePic, PasswordChangeSuccessView, EditPostView
+)
 from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    
     path("info", InfoPageView.as_view(), name="info"),
+
     path("rsvp", RSVPPageView.as_view(), name="rsvp"),
-    path("logout", LogoutView.as_view(), name="logout"),
+
     path("registry", RegistryListView.as_view(), name="registry"),
     path("reserve-item", RegistryPostView.as_view(), name="reserve-item"),
+
     path("forum", ThreadListView.as_view(), name="forum"),
     path("forum/create-thread", CreateThreadView.as_view(), name="create-thread"),
     path("forum/<slug:threadslug>", PostListView.as_view(), name="thread"),
     path("forum/<slug:threadslug>/<int:id>/edit", EditPostView.as_view(), name="edit-post"),
+
     path("account-info", AccountInfoView.as_view(), name="account-info"),
     path("profile-pic", ChangeProfilePic.as_view(), name="change-profile-pic"),
 
     path("login", auth_views.LoginView.as_view(), name="login"),
+    path("logout", LogoutView.as_view(), name="logout"),
 
     path("change-password", auth_views.PasswordChangeView.as_view(success_url=reverse_lazy("password-change-success"), template_name="change_password.html"), name="change-password"),
     path("password-change-success", PasswordChangeSuccessView.as_view(), name="password-change-success"),
