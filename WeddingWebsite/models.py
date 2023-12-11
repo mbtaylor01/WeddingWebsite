8537c8050cced645f42f4b75b82ab3c32fb7afb6
Mark Taylor <marktaylor1@protonmail.com>
2023-12-11 07:19:19 -0600

    Prettify and comment models.py

--- WeddingWebsite/models.py
+++ WeddingWebsite/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.urls import reverse
-from django.utils.text import slugify
 from django.utils import timezone
+from django.utils.text import slugify
 from django.contrib.auth.models import AbstractUser
 
+
 class RSVP(models.Model):
+    """
+    Model for an RSVP each user fills out.
+    """
     party_size = models.PositiveSmallIntegerField()
     allergies = models.CharField(max_length=1000, blank=True)
     alcohol = models.CharField(max_length=1000, blank=True)
@@ -15,22 +19,35 @@ class RSVP(models.Model):
         
 
 class CustomUser(AbstractUser):
+    """
+    A normal user object, but with profile_pic and rsvp added.
+    """
     profile_pic = models.ImageField(upload_to="profile_pics", default="profile_pics/default/default.png")
     rsvp = models.OneToOneField(RSVP, on_delete=models.SET_NULL, null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse("change-profile-pic")
 
+
 class RegistryEntry(models.Model):
+    """
+    Model for an item in the registry.
+    """
     title = models.CharField(max_length=500)
     image = models.ImageField(upload_to="images")
+    # users can click the image to go to the website where the item is sold
     image_link = models.URLField()
+    # users can reserve an item
     reserved_by = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
 
+
 class Thread(models.Model):
+    """
+    Model for a thread in the forum.
+    """
     title = models.CharField(max_length=500)
     creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     creation_time = models.DateTimeField(default=timezone.now, blank=True)
@@ -43,16 +60,25 @@ class Thread(models.Model):
     
     def __str__(self):
         return self.title
-    
+
+
 class Post(models.Model):
+    """
+    Model for a post in a thread.
+    """
     creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    # if a post is edited, "(edited)" will display at the end
     edited = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.id)
 
+
 class PostVersion(models.Model):
+    """
+    Model for a version of an individual post  Only the most recent version is displayed.
+    """
     text = models.TextField()
     creation_time = models.DateTimeField(default=timezone.now, blank=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
